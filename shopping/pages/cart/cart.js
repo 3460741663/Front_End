742be733e5ec03945c13dc03d1923432013d1b00
3460741663 <3460741663@qq.com>
2019-08-09 17:20:02 +0800

    购物车功能

--- shopping/pages/cart/cart.js
+++ shopping/pages/cart/cart.js
@@ -5,8 +5,106 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    hasList:false,
+    carts:[],
+    selectAllStatus:true,
+    totalPrice:''
   },
+  getTotalPrice(){
+    let carts = this.data.carts;
+    let total = 0;
+    for(let i = 0; i < carts.length; i++){
+      if (carts[i].selected){
+        total += carts[i].num * carts[i].price;
+      }
+    }
+    this.setData({
+      totalPrice:total.toFixed(2)
+    })
+  },
+  selectList(e){
+    let index = e.currentTarget.dataset.index;
+    //console.log(e);
+    let select = `carts[${index}].selected`
+    this.setData({
+      [select]:!this.data.carts[index].selected
+    });
+    this.getTotalPrice();
+    this.setSelectAll();
+  },
+  setSelectAll(){
+    let carts = this.data.carts;
+    let flag = true;
+    for (let i = 0; i < carts.length; i++) {
+      if (!carts[i].selected) {
+        flag = false;
+      }
+    }
+    if(flag){
+      this.setData({
+        selectAllStatus:true
+      })
+    }
+    else{
+      this.setData({
+        selectAllStatus: false
+      })
+    }
+  },
+  selectAll(){
+    let selectAllStatus = this.data.selectAllStatus;
+    selectAllStatus = !selectAllStatus;
+    let carts = this.data.carts;
+    for(let i = 0 ; i < carts.length; i++){
+      carts[i].selected = selectAllStatus
+    };
+    this.setData({
+      selectAllStatus:selectAllStatus,
+      carts : carts,
+    });
+    this.getTotalPrice();
+  },
+  deleteList(e){
+    // console.log(e);
+    const index = e.currentTarget.dataset.index;
+    let carts = this.data.carts;
+    carts.splice(index,1);
+    this.setData({
+      carts:carts,
+    })
+    if(!carts.length){
+      this.setData({
+        hasList:false,
+      })
+    }else{
+      this.getTotalPrice();
+    }
+  },
+  addCount(e){
+    const index = e.currentTarget.dataset.index;
+    let carts = this.data.carts;
+    let num = carts[index].num;
+    num++;
+    carts[index].num = num;
+    this.setData({
+      carts:carts
+    })
+    this.getTotalPrice();
+  },
+  subtractCount(e){
+    const index = e.currentTarget.dataset.index;
+    let carts = this.data.carts;
+    let num = carts[index].num;
+    if(num !=1){
+      num--;
+    }
+    carts[index].num = num;
+    this.setData({
+      carts: carts
+    })
+    this.getTotalPrice();
+  },
+  
 
   /**
    * 生命周期函数--监听页面加载
@@ -26,7 +124,17 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-
+    setTimeout(()=>{
+      this.setData({
+        hasList:true,
+        carts: [
+          { id: 1, title: '新鲜芹菜 半斤', image: '/image/s5.png', num: 4, price: 0.01, selected: true },
+          { id: 2, title: '素米 500g', image: '/image/s6.png', num: 1, price: 0.03, selected: true }
+        ],
+      })
+      this.getTotalPrice();
+    });
+    
   },
 
   /**
